0d08a2b8de1423c206d8b582b41b4a534d4c8ef4
Lucas Marques <dev.lucashmarques@outlook.com>
2023-11-06 22:00:38 -0300

    atualização

--- Lições/java/Projeto_P2/src/projeto/Projeto.java
+++ Lições/java/Projeto_P2/src/projeto/Projeto.java
@@ -1,20 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package projeto;
 
-/**
- *
- * @author lukasbom
- */
-public class Projeto {
+import view.Principal;
 
-    /**
-     * @param args the command line arguments
-     */
+public class Projeto {
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        new Principal().setVisible(true);
     }
     
 }
